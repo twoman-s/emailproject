faee9a96187092f8566ccdcb1eccdee42db0bd3f
AlexAllu <alexsasi545@gmail.com>
2020-10-07 21:30:28 +0530

    try except

--- demoapp/views.py
+++ demoapp/views.py
@@ -46,6 +46,6 @@ def sendanemail(request):
         email.send()
         data = {"status": "success"}
         return Response(data)
-    except:
-        data = {"status": "failed"}
+    except Exception as e:
+        data = {"status": e}
     return Response(data)
